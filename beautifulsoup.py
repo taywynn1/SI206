21a6365117e2e5abc4087b9d4b843a2c932989d9
Taylor Wynn <tmwynn@umich.edu>
2017-11-03 03:01:54 -0400

    Reworking class problems to study for midterm

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -4,5 +4,6 @@ url = input('Enter - ')
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')# Retrieve all of the anchor tags
 tags = soup('a')
+print (type(tags))
 for tag in tags:
 	print(tag.get('href', None))
